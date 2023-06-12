abec631f963096923d5b83e59d440efb8ba423d7
juancarbajal98 <juancarbajal98@gmail.com>
2023-06-12 08:45:15 -0700

    Card game

--- cardGame.js
+++ cardGame.js
@@ -27,3 +27,70 @@ kings = 13
 6. console log both card objects, and a message declaring which one has the higher value
 
 */
+
+class Deck {
+  // Initialize Deck
+  constructor(){
+    this.cards = []
+    this.suits = {
+      0:'hearts',
+      1:'diamonds',
+      2:'clubs',
+      3:'spades'
+    }
+    this.dynamicallyGenerateCards()
+  }
+
+  dynamicallyGenerateCards(){
+    // outer loop iterates over suits, inner loops iterates over numeric value
+    for(let suit=0; suit<4; suit++){
+      for(let val=1; val<14; val++){
+        this.cards.push({
+          suit: this.suits[suit],
+          value: val
+        })
+      }
+    }
+  }
+
+  // method to pull and remove a random card from the Deck
+  drawRandomCard(){
+    // get random index between [1, num of cards]
+    let randomIndex = Math.floor(Math.random() * ((this.cards.length-1) - 1 + 1) + 1)
+    //store random card before removal
+    let randomCard = this.cards[randomIndex] 
+    // remove random card from this.cards
+    this.cards.splice(randomIndex, 1) 
+    // return random card with that card now removed from the deck so future draw will not get repeat
+    return randomCard 
+  }
+};
+
+/*
+This simulation of the War card game:
+- is played with a single deck of cards
+- removes the first card that was pulled from the deck before the second card is pulled
+- compares and displays the cards and result in the browser console
+
+Assuming cards of different suits and equal numeric value have equal value, there are 3 cases for the result
+*/
+const warGame = (deck) => {
+  // draw cards
+  let randomCardOne = deck.drawRandomCard()
+  let randomCardTwo = deck.drawRandomCard()
+
+  // determine result
+  let result 
+  if(randomCardOne.value > randomCardTwo.value) result = 'Card One has the higer value.'
+  else if(randomCardOne.value < randomCardTwo.value) result = 'Card Two has the higer value.'
+  else result = 'The cards have equal value.'
+  
+  // output result
+  console.log(`Card One: ${JSON.stringify(randomCardOne)}`)
+  console.log(`Card Two: ${JSON.stringify(randomCardTwo)}`)
+  console.log(result)
+}
+
+// Run game (can view output by opening cardGame.html in web browser)
+let CardDeck = new Deck()
+warGame(CardDeck)
